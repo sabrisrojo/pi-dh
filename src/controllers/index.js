90fd353d8a3962b055f4ace360d7e1ccd22efde1
Sabrina Rojo <sabrinatatavitto@gmail.com>
2023-01-14 04:36:45 -0300

    Adiciona endpoints de teste para session e cookies

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const session = require("express-session")
 
 const index = {
   get: function (req, res) {
@@ -18,4 +19,44 @@ const index = {
   },
 }
 
-module.exports = { index }
+const cookies = {
+  get: function (req, res) {
+    console.log(req.cookies)
+    res.send(req.cookies)
+  },
+  post: function (req, res) {
+    res.cookie("lastAccessCookie", new Date(), { maxAge: 1000 * 60 * 60 * 24 })
+    console.log(req.cookies.lastAccessCookie)
+    res.send(req.cookies.lastAccessCookie)
+  },
+  put: function (req, res) {
+    res.send("PUT request to the cookies")
+  },
+  delete: function (req, res) {
+    res.cookie("lastAccessCookie", null, { maxAge: 0 })
+    console.log(req.cookies.lastAccessCookie)
+    res.send(req.cookies.lastAccessCookie)
+  },
+}
+
+const sessions = {
+  get: function (req, res) {
+    console.log(req.session)
+    res.send(req.session)
+  },
+  post: function (req, res) {
+    req.session.lastAccessSession = new Date()
+    console.log(req.session.lastAccessSession)
+    res.send(req.session.lastAccessSession)
+  },
+  put: function (req, res) {
+    res.send("PUT request to the cookies")
+  },
+  delete: function (req, res) {
+    req.session.lastAccessSession = null
+    console.log(req.session.lastAccessSession)
+    res.send(req.session.lastAccessSession)
+  },
+}
+
+module.exports = { index, cookies, sessions }
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,4 +11,14 @@ router.post("/", indexController.index.post)
 router.put("/", indexController.index.put)
 router.delete("/", indexController.index.delete)
 
+router.get("/cookies", indexController.cookies.get)
+router.post("/cookies", indexController.cookies.post)
+router.put("/cookies", indexController.cookies.put)
+router.delete("/cookies", indexController.cookies.delete)
+
+router.get("/session", indexController.sessions.get)
+router.post("/session", indexController.sessions.post)
+router.put("/session", indexController.sessions.put)
+router.delete("/session", indexController.sessions.delete)
+
 module.exports = router
